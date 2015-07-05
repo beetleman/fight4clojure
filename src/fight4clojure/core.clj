844aad81f877c956f2a82ee7ea6ba4dc3dd5d8f0
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2015-07-05 15:21:02 +0200

    first 20 problems

--- src/fight4clojure/core.clj
+++ src/fight4clojure/core.clj
@@ -4,4 +4,89 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Run tests!"))
+
+
+(def problem-1
+  true)
+
+
+(def problem-2
+  4)
+
+
+(def problem-3
+  "HELLO WORLD")
+
+
+;; problem-4
+;; :a :b :c
+
+
+(def problem-5
+  '(1 2 3 4))
+
+
+;; problem-5
+;; :a :b :c
+
+
+(def problem-7
+  [1 2 3 4])
+
+
+(def problem-8
+  #{:a :b :c :d})
+
+
+(def problem-9
+  2)
+
+
+(def problem-9
+  2)
+
+
+(def problem-10
+  20)
+
+
+(def problem-11
+  [:b 2])
+
+
+(def problem-12
+  3)
+
+
+(def problem-13
+  [20 30 40])
+
+
+(def problem-14
+  8)
+
+
+(def problem-15
+  #(* 2 %))
+
+
+(def problem-16
+  #(str "Hello, " % "!"))
+
+
+(def problem-17
+  '(6 7 8))
+
+
+(def problem-18
+  '(6 7))
+
+
+(def problem-19
+  #(first (reverse %)))
+
+
+(def problem-20
+  (comp second reverse))
+

59095da358597c08314cf5a9a7ef97cf3bd5d2a5
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2015-07-05 18:27:55 +0200

    next 20 problems

--- src/fight4clojure/core.clj
+++ src/fight4clojure/core.clj
@@ -200,3 +200,215 @@
 
 (def problem-40
   (fn [sep xs] (drop-last (mapcat #(list % sep) xs))))
+
+
+(def problem-41
+  (fn [xs n] 
+    (mapcat (partial take (dec n)) 
+            (partition-all n xs))))
+
+
+(def problem-42
+  (fn [n] 
+    (if (>= 1 n ) 
+      n 
+      (apply * (range 1 (inc n))))))
+
+
+(def problem-43
+  #(apply map list (partition %2 %1)))
+
+
+(def problem-44
+  (fn [step sx]
+    (let [len (count sx)
+          step (mod step len)
+          sx (cycle sx)]
+      (take len (drop step sx)))))
+
+
+(def problem-45
+  [1 4 7 10 13])
+
+
+(def problem-46
+  (fn [f]
+    (fn [x y] (f y x))))
+
+
+(def problem-47
+  4)
+
+
+(def problem-48
+  6)
+
+
+(def problem-49
+  (fn [n xs] (list (take n xs) (drop n xs))))
+
+
+(def problem-50
+  (comp vals (partial group-by type)))
+
+
+(def problem-51
+  [1 2 3 4 5])
+
+
+;; problem-52
+;; [c e]
+
+
+(def problem-53
+  (fn [[x & xs]]
+    (let [longest (-> (reduce (fn [[g & grests :as gs] cur]
+                                (if (= (last g) (dec cur))
+                                  (cons (conj g cur) grests)
+                                  (cons [cur] gs)))
+                              (list [x]) xs)
+                      sort
+                      last)]
+      (if (= (count longest) 1)
+        []
+        longest))))
+
+
+(def problem-54
+  (fn [n xs]
+    (loop [n n
+           xs xs
+           partitioned []]
+      (if (empty? xs)
+        (filter #(= (count %) n) partitioned)
+        (recur n (drop n xs) (conj partitioned (take n xs)))))))
+
+
+(def problem-55
+  (fn [xs]
+    (apply hash-map
+           (mapcat
+            (fn [[k v]] [k (count v)])
+            (group-by identity xs)))))
+
+
+(def problem-56
+  (fn [xs]
+    (reduce
+     (fn [reduced x]
+       (if (some #(= % x) reduced)
+         reduced
+         (conj reduced x)))
+     [] xs)))
+
+
+(def problem-57
+  [5 4 3 2 1])
+
+
+(def problem-58
+  (fn [f & fs]
+    (reduce
+     (fn [rf f] (fn [& x] (rf (apply f x ))))
+     f fs)))
+
+
+(def problem-59
+  (fn [& fs]
+    (fn [& xs]
+      (map (fn [f] (apply f xs)) fs))))
+
+
+(def problem-60
+  (fn reductions*
+    ([f xs] (reductions* f (first xs) (next xs)))
+    ([f val xs]
+     (let [to-take (iterate inc 0)]
+       (map
+        (fn [x y] (reduce f val (take x xs)))
+        to-take (cons val xs))))))
+
+
+(def problem-61
+  #(apply hash-map (mapcat list %1 %2)))
+
+
+(def problem-62
+  (fn *iterate [f x]
+    (cons x (lazy-seq (*iterate f (f x))))))
+
+
+(def problem-63
+  (fn [f xs]
+    (reduce (fn [vs x]
+              (let [key (f x)]
+                (assoc vs key (conj (vs key []) x))))
+            {} xs)))
+
+
+(def problem-64
+  +)
+
+
+(def problem-65
+  (fn [x]
+    (let [v [:key :value]]
+      (cond
+        (= :value (get (conj x v) :key))
+        :map
+        (= (conj x v) (conj x v v))
+        :set
+        (reversible? x)
+        :vector
+        :else
+        :list))))
+
+
+(def problem-66
+  (fn [x y]
+    (let [[x y] (sort [x y])]
+      (loop [x x
+             y y
+             acc x]
+        (if (= 0 (mod y acc) (mod x acc))
+          acc
+          (recur x y (dec acc)))))))
+
+
+(def problem-67
+  (fn *prime [n]
+    (loop [acc []
+           curr 2]
+      (cond
+        (= (count acc) n)
+        acc
+        (not-any? zero? (map #(mod curr %) acc))
+        (recur (conj acc curr) (inc curr))
+        :else
+        (recur acc (inc curr))))))
+
+
+(def problem-68
+  [7 6 5 4 3])
+
+
+(def problem-69
+  (fn [f & maps]
+    (loop [acc {}
+           maps maps]
+      (if (empty? maps)
+        acc
+        (recur
+         (reduce
+          (fn [acc [key val]]
+            (if (contains? acc key)
+              (assoc acc key (f (acc key) val))
+              (assoc acc key val)))
+          acc (first maps))
+         (rest maps))))))
+
+
+(def problem-70
+  (fn [s]
+    (sort-by #(clojure.string/lower-case %)
+             (clojure.string/split s #"\W"))))
